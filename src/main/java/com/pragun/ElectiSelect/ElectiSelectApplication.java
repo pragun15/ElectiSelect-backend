b5365c49fb44c72e1ac24a0da4d6b29ae58a07dd
Pragun <pragunpradeep2@gmail.com>
2026-06-05 09:28:18 +0530

    Remove dotenv dependency for Render deployment

--- src/main/java/com/pragun/ElectiSelect/ElectiSelectApplication.java
+++ src/main/java/com/pragun/ElectiSelect/ElectiSelectApplication.java
@@ -1,19 +1,13 @@
 package com.pragun.ElectiSelect;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ElectiSelectApplication {
 
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
-		SpringApplication.run(ElectiSelectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ElectiSelectApplication.class, args);
+    }
 
 }
