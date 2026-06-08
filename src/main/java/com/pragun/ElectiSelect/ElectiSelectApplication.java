95d847786fe81183a411128d4df09a3e0e0a1cb1
Pragun <pragunpradeep2@gmail.com>
2026-06-08 17:42:27 +0530

    changed the timezone.

--- src/main/java/com/pragun/ElectiSelect/ElectiSelectApplication.java
+++ src/main/java/com/pragun/ElectiSelect/ElectiSelectApplication.java
@@ -3,10 +3,13 @@ package com.pragun.ElectiSelect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ElectiSelectApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(ElectiSelectApplication.class, args);
     }
 
